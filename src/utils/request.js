583ddf45e439aaa48255a983ed0e36146e38196d
yucc <l>
2020-06-12 19:32:51 +0800

    修改响应时长

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ const service = axios.create({
 //   baseURL: '/api',
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 15000 // request timeout
 })
 
 // request interceptor
